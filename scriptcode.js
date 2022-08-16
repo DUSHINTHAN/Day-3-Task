c18bd7802e85d558083fc5a07c57353c38202985
DUSHINTHAN <110904924+DUSHINTHAN@users.noreply.github.com>
2022-08-17 01:15:40 +0530

    Add files via upload

---
+++ scriptcode.js
@@ -0,0 +1,36 @@
+let myself= "BioData:"
+console.log(myself)
+
+let person={
+
+    name: "Dushinthan K",
+    email: "vankatesh.dushinthan@gmail.com",
+    age: 25,
+    Residence: "Chennai",
+    Education:{
+        Degree: "Bachelor of Engineering",
+        Departmant: "Mehcanical",
+        Completion: "2019",
+    },
+    Work:{
+
+        Organisation: "Amazon Development Center",
+        Experience: "2 yers and 8 months",
+        Location: "Chennai",
+     },
+
+     Hobbies:{
+
+        Hobby1: "playing outdoor sports",
+        Hobby2: "Playing online Video Game"
+    },
+
+}
+
+console.log(person);
+console.log(person.Work);
+console.log(person.Hobbies);
+
+
+let Declaration= "I hereby declare that all the above given information are true to my knowledge" 
+console.log(Declaration);
